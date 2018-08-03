5ed7e006d96259f491e3552feb891848a50161c7
Hongjeongmin <wjdals0471@naver.com>
2018-08-03 19:50:41 +0900

    fff
    fff

---
+++ algorithm/3586.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int value()
+{
+	string tt;
+	while(true)
+	{
+
+	}
+}
+
+int main(void)
+{
+	string str;
+	cin>>str;
+	
+	return 0;
+}
