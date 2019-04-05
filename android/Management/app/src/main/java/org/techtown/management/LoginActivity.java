631aaf8ea33dfdfa59774b5878bd3adfadbd6ac1
Redbin <30316976+Hongjeongmin@users.noreply.github.com>
2019-04-06 01:42:52 +0900

    android 회원목록 출력 미 버튼 숨기기
    ㅇㅇ

--- android/Management/app/src/main/java/org/techtown/management/LoginActivity.java
+++ android/Management/app/src/main/java/org/techtown/management/LoginActivity.java
@@ -1,12 +1,21 @@
 package org.techtown.management;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.security.spec.ECField;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,11 +24,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText idText = (EditText) findViewById(R.id.idText);
-        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final TextView idText = (TextView) findViewById(R.id.idText);
+        final TextView passwordText = (TextView) findViewById(R.id.passwordText);
 
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        TextView registerButton = (TextView) findViewById(R.id.registerButton);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        final TextView registerButton = (TextView) findViewById(R.id.registerButton);
 
         //버튼을 눌럿을때 이벤트 처리
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +37,49 @@ public class LoginActivity extends AppCompatActivity {
                 //레지스트 인텐트로 넘어갈수록 해준다
                 Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                final String userID = idText.getText().toString();
+                final String userPassword = passwordText.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success =jsonResponse.getBoolean("success");
+                            if(success){
+                                String userID = jsonResponse.getString("userID");
+                                String userPassword = jsonResponse.getString("userPassword");
+                                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                                intent.putExtra("userID",userID);
+                                intent.putExtra("userPassword",userPassword);
+                                LoginActivity.this.startActivity(intent);
+                            }
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("로그인에 실패하였습니다.")
+                                        .setNegativeButton("다시 시도",null)
+                                        .create()
+                                        .show();
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(userID,userPassword,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+
             }
         });
     }
--- android/Management/app/src/main/java/org/techtown/management/MainActivity.java
+++ android/Management/app/src/main/java/org/techtown/management/MainActivity.java
@@ -1,9 +1,19 @@
 package org.techtown.management;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +22,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText idText = (EditText) findViewById(R.id.idText);
-        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        TextView idText = (TextView) findViewById(R.id.idText);
+        TextView passwordText = (TextView) findViewById(R.id.passwordText);
         TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
+        Button managementButton = (Button) findViewById(R.id.managementButton);
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPassword = intent.getStringExtra("userPassword");
+        String message = "환영합니다, " + userID + "님!";
+
+        idText.setText(userID);
+        passwordText.setText(userPassword);
+        welcomeMessage.setText(message);
+
+        if(!userID.equals("admin"))
+        {
+            //managementButton.setEnabled(false);
+            managementButton.setVisibility(View.GONE);
+        }
+
+        managementButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                new BackgroundTask().execute();
+            }
+        });
+
+    }
+    //내부클래스
+    class BackgroundTask extends AsyncTask<Void,Void,String>
+    {
+        String target;
+
+        @Override
+        protected void onPreExecute(){
+            target = "http://pssin1.cafe24.com/List.php";
+            //초기화하는부분
+        }
+
+        @Override
+        protected String doInBackground(Void... voids){
+            try{
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((temp = bufferedReader.readLine()) !=null)
+                {
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                //해당문자열의 집합 반환
+                return stringBuilder.toString().trim();
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            //오류발생시 null
+            return null;
+        }
+        @Override
+        public  void onProgressUpdate(Void... values){
+            super.onProgressUpdate(values);
+        }
+
+
+        @Override
+        public void onPostExecute(String result){
+            Intent intent = new Intent(MainActivity.this,ManagementActivity.class);
+            intent.putExtra("userList",result);
+            MainActivity.this.startActivity(intent);
+        }
     }
 }
